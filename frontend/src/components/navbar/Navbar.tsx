a8fa40d348d4a15cf7d14fa2eb5d29023a4fbfee
Maksim Marychev <marycheff17@gmail.com>
2024-11-11 16:50:02 +0400

    27:35

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
             <h2 className="text-xl font-semibold text-black py-2">Заметки</h2>
-            <ProfileInfo />
+            <ProfileInfo  />
         </div>
     )
 }
